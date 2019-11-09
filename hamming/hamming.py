12987845c6831c3a3009b1df111d67d51a0c5264
Sinan Ata <sinanata@gmail.com>
2019-11-09 20:03:06 +0300

    Completed the hamming challenge

--- hamming/hamming.py
+++ hamming/hamming.py
@@ -1,8 +1,6 @@
 def distance(strand_a, strand_b):
-
     if (len(strand_a) == len(strand_b)):
         return sum ( strand_a[i] != strand_b[i] for i in range(len(strand_a)) )
     else:
-        self.exception = r".+"
-
-    pass
+        print("Something went wrong with these: ", strand_a, strand_b)
+        raise ValueError("Something went wrong with these: ", strand_a, strand_b)
