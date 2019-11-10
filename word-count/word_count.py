cb523422f8aeee500b0ee127457b6da0489658b5
Sinan Ata <sinanata@gmail.com>
2019-11-11 01:10:32 +0300

    Finished working on the word-count challenge

--- word-count/word_count.py
+++ word-count/word_count.py
@@ -1,2 +1,8 @@
+from collections import *
+import re
 def count_words(sentence):
-    pass
+    sentence = re.sub("_", " ", sentence.lower())
+    inspected = re.findall(r"([\w]+[']?[\w]+|\d|[a-z])", sentence.lower())
+    
+    return Counter(inspected)
+pass
