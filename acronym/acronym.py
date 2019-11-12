8a18e2888077619bf3ceb554d77f94e7e15f72e0
Sinan Ata <sinanata@gmail.com>
2019-11-12 14:51:48 +0300

    Finished working on the acronym challenge, tried optimizing code for word_count

--- acronym/acronym.py
+++ acronym/acronym.py
@@ -1,2 +1,9 @@
+import re
 def abbreviate(words):
-    pass
+    
+    words = re.sub("_|-", " ", words)
+    inspected = re.findall(r"([\w]+[']?[\w]+|\d|[a-zA-Z])", words)
+
+    acronym = ''.join(word[0] for word in inspected)
+    
+    return acronym.upper()
