c7872d53b3945a2d2918da4e9427fefb46871903
Sinan Ata <sinanata@gmail.com>
2019-11-14 18:14:57 +0300

    Finished working on kindergarten-garden challenge

--- kindergarten-garden/kindergarten_garden.py
+++ kindergarten-garden/kindergarten_garden.py
@@ -1,3 +1,19 @@
-class Garden:
-    def __init__(self, diagram, students):
-        pass
+STUDENT_LIST = ['Alice', 'Bob', 'Charlie', 'David', 'Eve',
+        'Fred', 'Ginny', 'Harriet','Ileana', 'Joseph',
+        'Kincaid', 'Larry']
+
+PLANTS = {'V': 'Violets', 'C': 'Clover', 'R': 'Radishes', 'G': 'Grass'}
+
+class Garden(object):
+    def __init__(self, diagram, students=STUDENT_LIST):
+        split_diagram = diagram.split('\n')
+        self.row1 = split_diagram[0]
+        self.row2 = split_diagram[1]
+        self.students = students
+        self.students.sort()
+
+    def plants(self, student):
+        n = self.students.index(student) * 2
+        r1 = self.row1
+        r2 = self.row2
+        return [PLANTS[r1[n]], PLANTS[r1[n+1]], PLANTS[r2[n]], PLANTS[r2[n+1]]]
