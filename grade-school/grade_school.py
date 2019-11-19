5edade5b4381fb0bfbe9384279b3cd81e8a4432a
Sinan Ata <sinanata@gmail.com>
2019-11-19 14:33:51 +0300

    working on the grade-school challenge

--- grade-school/grade_school.py
+++ grade-school/grade_school.py
@@ -1,12 +1,28 @@
 class School:
     def __init__(self):
+
+        self.student_dict = {}
+
         pass
 
     def add_student(self, name, grade):
+
+        self.student_dict.__setitem__(name, grade)
+
         pass
 
     def roster(self):
+
+        return [k for k in self.student_dict.keys()]
+
         pass
 
     def grade(self, grade_number):
+
+        output = []
+
+        list_of_keys = [key for (key, value) in self.student_dict.items() if value == grade_number]
+
+        return list_of_keys
+
         pass
