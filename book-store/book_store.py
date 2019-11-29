5795f695fa420051f0d2dbecb50fc0bb118d40e9
Sinan Ata <sinanata@gmail.com>
2019-11-29 17:23:52 +0300

    Finished working on the book-store challenge, started working on the leap challenge

--- book-store/book_store.py
+++ book-store/book_store.py
@@ -1,55 +1,30 @@
-"""
-One copy of any of the five books costs $8.
+from collections import Counter
 
-If, however, you buy two different books, you get a 5% discount on those two books.
+PRICE = 800
 
-If you buy 3 different books, you get a 10% discount.
-
-If you buy 4 different books, you get a 20% discount.
-
-If you buy all 5, you get a 25% discount.
-
-Note: that if you buy four books, of which 3 are different titles, you get a 10% discount on the 3 that form part of a set, but the fourth book still costs $8.
-
-Your mission is to write a piece of code to calculate the price of any conceivable shopping basket (containing only books of the same series), giving as big a discount as possible.
-
-For example, how much does this basket of books cost?
-
-2 copies of the first book
-2 copies of the second book
-2 copies of the third book
-1 copy of the fourth book
-1 copy of the fifth book
-One way of grouping these 8 books is:
-
-1 group of 5 --> 25% discount (1st,2nd,3rd,4th,5th)
-+1 group of 3 --> 10% discount (1st,2nd,3rd)
-This would give a total of:
-
-5 books at a 25% discount
-+3 books at a 10% discount
-Resulting in:
-
-5 x (8 - 2.00) == 5 x 6.00 == $30.00
-+3 x (8 - 0.80) == 3 x 7.20 == $21.60
-For a total of $51.60
-
-However, a different way to group these 8 books is:
-
-1 group of 4 books --> 20% discount (1st,2nd,3rd,4th)
-+1 group of 4 books --> 20% discount (1st,2nd,3rd,5th)
-This would give a total of:
-
-4 books at a 20% discount
-+4 books at a 20% discount
-Resulting in:
-
-4 x (8 - 1.60) == 4 x 6.40 == $25.60
-+4 x (8 - 1.60) == 4 x 6.40 == $25.60
-For a total of $51.20
-
-And $51.20 is the price with the biggest discount.
-"""
+PRICE_PER_GROUP = {
+    1: 1 * PRICE * 1.00,
+    2: 2 * PRICE * 0.95,
+    3: 3 * PRICE * 0.90,
+    4: 4 * PRICE * 0.80,
+    5: 5 * PRICE * 0.75,
+}
 
 def total(basket):
+    if not basket:
+        return 0
+
+    books_collection = Counter(basket)
+    prices = [PRICE * len(basket)]
+
+    for max_group_size in range(2, len(books_collection) + 1):
+        remaining_books_in_collection = books_collection.copy()
+        prices.append(0)
+        while remaining_books_in_collection:
+            books_group_collection = Counter(b[0] for b in remaining_books_in_collection.most_common(max_group_size))
+            prices[max_group_size - 1] += PRICE_PER_GROUP[len(books_group_collection)]
+            remaining_books_in_collection -= books_group_collection
+
+    return min(prices)
+
     pass
