757053eef742161635c6721a7ef5a972dc513f5b
Sinan Ata <sinanata@gmail.com>
2019-11-30 01:48:45 +0300

    Finished working on the leap challenge, started working on the rest_api challenge

--- leap/leap.py
+++ leap/leap.py
@@ -1,2 +1,12 @@
 def leap_year(year):
+
+    if year%4 != 0: 
+        result = False
+    elif year%100 != 0:
+        result = True
+    elif year%400 != 0:
+        result = False
+    else:
+        result = True
+    return result
     pass
