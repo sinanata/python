5c680751fb8c1848e32f178cfea4a160db59b236
Sinan Ata <sinanata@gmail.com>
2019-11-30 18:39:31 +0300

    Finished working on the ISBN Verifier challenge

--- isbn-verifier/isbn_verifier.py
+++ isbn-verifier/isbn_verifier.py
@@ -1,2 +1,13 @@
+import re
+
 def is_valid(isbn):
-    pass
+    isbn = isbn.replace("-", "").replace(" ", "").upper();
+    match = re.search(r'^(\d{9})(\d|X)$', isbn)
+    if not match:
+        return False
+
+    digits = match.group(1)
+    check_digit = 10 if match.group(2) == 'X' else int(match.group(2))
+
+    result = sum((i + 1) * int(digit) for i, digit in enumerate(digits))
+    return (result % 11) == check_digit
