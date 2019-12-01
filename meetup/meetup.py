5639a4924394b3fd3731f72f99f93d4a608b9b74
Sinan Ata <sinanata@gmail.com>
2019-12-01 22:47:03 +0300

    Working on the meetup challenge

--- meetup/meetup.py
+++ meetup/meetup.py
@@ -1,2 +1,19 @@
+from datetime import date, timedelta
+
+def daterange(start_date, end_date):
+        for n in range(int ((end_date - start_date).days)):
+            yield start_date + timedelta(n)
+
 def meetup(year, month, week, day_of_week):
+
+    start_date = date(year, month, 13)
+    end_date = date(year, month, 20)
+
+    for single_date in daterange(start_date, end_date):
+        if single_date.strftime("%A") == day_of_week:
+            return single_date
+
+
+
+def MeetupDayException():
     pass
