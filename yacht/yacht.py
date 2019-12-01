354bf9c23b2866dc6912f628f301fbbd1b473365
Sinan Ata <sinanata@gmail.com>
2019-12-01 16:37:28 +0300

    Finished working on the yacht challenge

--- yacht/yacht.py
+++ yacht/yacht.py
@@ -1,32 +1,41 @@
-"""
-This exercise stub and the test suite contain several enumerated constants.
+from collections import Counter
 
-Since Python 2 does not have the enum module, the idiomatic way to write
-enumerated constants has traditionally been a NAME assigned to an arbitrary,
-but unique value. An integer is traditionally used because it’s memory
-efficient.
-It is a common practice to export both constants and functions that work with
-those constants (ex. the constants in the os, subprocess and re modules).
+def YACHT(x):
+    return 50 if len(set(x)) == 1 else 0
 
-You can learn more here: https://en.wikipedia.org/wiki/Enumerated_type
-"""
+def ONES(x):
+    return sum([i for i in x if i == 1])
 
+def TWOS(x):
+    return sum([i for i in x if i == 2])
 
-# Score categories.
-# Change the values as you see fit.
-YACHT = None
-ONES = None
-TWOS = None
-THREES = None
-FOURS = None
-FIVES = None
-SIXES = None
-FULL_HOUSE = None
-FOUR_OF_A_KIND = None
-LITTLE_STRAIGHT = None
-BIG_STRAIGHT = None
-CHOICE = None
+def THREES(x):
+    return sum([i for i in x if i == 3])
 
+def FOURS(x):
+    return sum([i for i in x if i == 4])
+
+def FIVES(x):
+    return sum([i for i in x if i == 5])
+
+def SIXES(x):
+    return sum([i for i in x if i == 6])
+
+def FULL_HOUSE(x):
+    return sum(x) if sorted(Counter(x).values()) == [2, 3] else 0
+
+def FOUR_OF_A_KIND(x):
+    four_times_elements = [dice for dice in set(x) if x.count(dice) >= 4]
+    return 4*four_times_elements[0] if len(four_times_elements) == 1 else 0
+
+def LITTLE_STRAIGHT(x):
+    return 30 if sorted(x) == [1, 2, 3, 4, 5] else 0
+
+def BIG_STRAIGHT(x):
+    return 30 if sorted(x) == [2, 3, 4, 5, 6] else 0
+
+def CHOICE(x):
+    return sum(x)
 
 def score(dice, category):
-    pass
+    return category(dice)
