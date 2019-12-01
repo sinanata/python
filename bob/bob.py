c47d84a74d393d7410d9fb5a91c5b370955819ef
Sinan Ata <sinanata@gmail.com>
2019-12-01 16:28:20 +0300

    Finished working on the annoying teenager, bob challenge

--- bob/bob.py
+++ bob/bob.py
@@ -1,2 +1,15 @@
+import re
 def response(hey_bob):
-    pass
+
+    cleared = str(re.findall(r'\b[a-zA-Z]+\b', hey_bob))
+
+    if cleared.isupper() == False and hey_bob.rstrip().endswith('?'):
+        return 'Sure.'
+    elif hey_bob.rstrip().endswith('?') and cleared.isupper() == True:
+        return 'Calm down, I know what I\'m doing!'
+    elif cleared.isupper() == True:
+        return 'Whoa, chill out!'
+    elif hey_bob.rstrip() == '':
+        return 'Fine. Be that way!'
+    else:
+        return 'Whatever.'
