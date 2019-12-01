e5d9e82140aeb1110ab58f093eefbe01cc5b31bd
Sinan Ata <sinanata@gmail.com>
2019-12-01 17:26:43 +0300

    Finished working on the run-length-encoding challenge

--- run-length-encoding/run_length_encoding.py
+++ run-length-encoding/run_length_encoding.py
@@ -1,6 +1,9 @@
-def decode(string):
-    pass
-
+from re import sub
 
 def encode(string):
-    pass
+    return sub(r'(.)\1*', lambda s: str( len(s.group(0)) if len(s.group(0)) > 1 else '' ) + s.group(1),
+               string)
+
+def decode(string):
+    return sub(r'(\d+)(\D)', lambda s: s.group(2) * int(s.group(1)),
+               string)
