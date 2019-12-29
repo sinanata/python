bbd8481c4d7b29f9e300c974c427270752996b7b
Sinan Ata <sinanata@gmail.com>
2019-12-29 15:28:16 +0300

    Finished working on the difference-of-squares challenge

--- difference-of-squares/difference_of_squares.py
+++ difference-of-squares/difference_of_squares.py
@@ -1,10 +1,11 @@
 def square_of_sum(number):
-    pass
+    return sum(range(1, number+1)) ** 2
 
 
 def sum_of_squares(number):
-    pass
+    return sum([i**2 for i in range(1, number+1)])
+    
 
 
 def difference_of_squares(number):
-    pass
+    return square_of_sum(number) - sum_of_squares(number)
