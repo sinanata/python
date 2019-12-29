43ed0c0ed37acb828e186f6c8323f47c5d83d342
Sinan Ata <sinanata@gmail.com>
2019-12-29 15:19:58 +0300

    Finished working on the rotational-cipher challenge

--- rotational-cipher/rotational_cipher.py
+++ rotational-cipher/rotational_cipher.py
@@ -1,2 +1,18 @@
 def rotate(text, key):
-    pass
+
+    result = ""
+    # transverse the plain text
+    for i in range(len(text)):
+        char = text[i]
+        # Encrypt uppercase characters in plain text
+        
+        if (char.isalpha() == False):
+            result += char
+
+        elif (char.isupper()):
+            result += chr((ord(char) + key - 65) % 26 + 65)
+
+        else:
+            result += chr((ord(char) + key - 97) % 26 + 97)
+    
+    return result
