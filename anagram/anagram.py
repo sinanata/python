bb21e2742b3f758eb8e717ce47728e583e40a62a
Sinan Ata <sinanata@gmail.com>
2020-01-01 15:36:44 +0300

    Finished working on the anagram challenge

--- anagram/anagram.py
+++ anagram/anagram.py
@@ -1,2 +1,2 @@
-def find_anagrams(word, candidates):
-    pass
+from collections import Counter
+def find_anagrams(word, candidates): return [item for item in candidates if sorted(item.lower()) == sorted(word.lower()) and item.lower() != word.lower()]
