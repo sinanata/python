5e7609ca52ca63992397e6dbf46ade3b91a07c09
Sinan Ata <sinanata@gmail.com>
2020-01-02 13:38:41 +0300

    Finished working on the allergies challenge, interesting binary method

--- allergies/allergies.py
+++ allergies/allergies.py
@@ -1,11 +1,32 @@
-class Allergies:
+allergies = {
+    1: 'eggs', #binary number 1
+    2: 'peanuts', #binary number 10
+    4: 'shellfish', #binary number 100
+    8: 'strawberries', #binary number 1000
+    16: 'tomatoes', #binary number 10000
+    32: 'chocolate', #binary number 100000
+    64: 'pollen', #binary number 1000000
+    128: 'cats' #binary number 10000000
+}
 
+class Allergies(object):
     def __init__(self, score):
-        pass
+        """Calculates allergies for given score."""
+        self.score = score
+        self.lst = []
+        self.__calculate_allergies()
 
-    def allergic_to(self, item):
-        pass
+    def __calculate_allergies(self):
+        value = 1
+        binary = "{:b}".format(self.score).zfill(8)
 
-    @property
-    def lst(self):
-        pass
+        for i in binary[::-1]:
+            if value >= 256:
+                break
+            if i == '1':
+                self.lst.append(allergies[value])
+            value <<= 1
+
+    def allergic_to(self, food):
+        """Check if person is allergic on given food."""
+        return food in self.lst
